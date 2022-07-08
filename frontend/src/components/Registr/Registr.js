cbf31641ae7cf0a8930035b00c41e8220dcedb6c
Nikolay <jump9207@gmail.com>
2022-07-08 12:07:46 +0300

    add auth, setting error message auth

--- frontend/src/components/Registr/Registr.js
+++ frontend/src/components/Registr/Registr.js
@@ -1,16 +1,47 @@
 import React from 'react';
-// import Box from '@mui/material/Box';
 import {TextField, Button} from '@mui/material';
+import {useDispatch} from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { AUTH_REGISTR } from "../../store/auth/actionsTypes";
 import './Registr.css';
 
 
 function Register() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = React.useCallback((event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const surname = event.target.surname.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    fetch("/register", {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        surname,
+        email,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        // проверка что мы залогинились
+        dispatch({ type: AUTH_REGISTR, payload: { id: data.id, login: data.login } });
+        navigate('/');
+    });
+  }, [dispatch, navigate]);
   return (
     <section className='Registration-section'>
       <div className='container'>
         <div className='Registration'>
           <h2 className='Registration-title'>Регистрация</h2>
-          <form className='Registration-form'>
+          <form className='Registration-form' onSubmit={handleSubmit}>
             <TextField
               type="text"
               label="name"
--- frontend/src/store/auth/actionsTypes.js
+++ frontend/src/store/auth/actionsTypes.js
@@ -1,1 +1,3 @@
 export const AUTH_LOGIN = 'AUTH_LOGIN';
+export const AUTH_REGISTR = 'AUTH_REGISTR';
+export const AUTH_ERROR= 'AUTH_ERROR';
