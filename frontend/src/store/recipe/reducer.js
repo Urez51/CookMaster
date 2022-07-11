44d057bdd4c72de241028e85a018aa3f858039a8
Pavel <gomer_74@mail.ru>
2022-07-11 14:30:02 +0300

    fixLkAdmin, publishRecipeButton

--- frontend/src/store/recipe/reducer.js
+++ frontend/src/store/recipe/reducer.js
@@ -1,8 +1,9 @@
-import { GET_MY_RECIPES,ADD_RECIPE, ERROR_MASSAGE_POST_RECIPE,DELETE_ERROR_MASSEGE } from './actionsTypes'
+import { GET_MY_RECIPES,ADD_RECIPE, ERROR_MASSAGE_POST_RECIPE,DELETE_ERROR_MASSEGE, ADMIN_PUBLICK_RECIPE } from './actionsTypes'
 
 
 const initialState = {
   recipes: [],
+  publicRecipe: [],
   newRecipeMessage:'', 
 };
 
@@ -19,6 +20,9 @@ export default function recipeReducer(state = initialState, action) {
     }
     case ADD_RECIPE:{
       return { ...state, newRecipeMessage: action.payload}
+    }
+    case ADMIN_PUBLICK_RECIPE: {
+      return {...state, publicRecipe: [...state.publicRecipe, action.payload] }
     }
     default: return state;
   }
