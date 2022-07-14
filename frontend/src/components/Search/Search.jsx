eef84945f372f0b702df545dafafd867ccf48bcf
Ivan Vakhaev <ivan_vakhaev@MacBook-Pro-Ivan.local>
2022-07-14 19:40:09 +0300

    fix bags with newRecipe

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Input } from '@mui/material';
+import { Autocomplete, Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Input, TextField } from '@mui/material';
 import { v4 as uuidv4 } from 'uuid';
 import { searchRecipe } from '../../store/searchRecipe/actionsCreators'
 import { useNavigate } from 'react-router-dom'
+import { getAllIngridients } from "../../store/ingridients and stap/actionsCreators";
 
 function Search() {
   const recipe = useSelector((state) => state.searchRecipe.searchRecipe);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
+  const {allIngridients} = useSelector((state)=>state.ingridientsAndSteps)
   const handleSearch = (event) => {
     event.preventDefault();
     const title = event.target.title.value.toLowerCase().trim();
     dispatch(searchRecipe(title));
   }
-
+  const [selectIngridient,setselectIngridient] = React.useState()
+  const handlerInputIng = React.useCallback((event)=>{
+    const wow = JSON.parse(event.target.getAttribute('data-value'))
+    setselectIngridient (wow)
+  })
+  useEffect(()=>{
+    dispatch(getAllIngridients())
+  },[])
+  const defaultProps = {
+    options: allIngridients,
+    getOptionLabel: (option) => (option.name + " " + "(" + option.measure + ")"), 
+  }
   return (
     <div style={{ "display": 'flex', "flexDirection": 'column', "alignItems": 'center', "marginTop": '5%' }}>
         <form onSubmit={handleSearch}>
@@ -40,7 +52,30 @@ function Search() {
             </Button>
           </Accordion>
         </form>
-
+        <form className='Form-add-ingridients Form-add' >
+            <TextField
+            type="number"
+            label="Количество"
+            name=""
+            variant="outlined"
+            className='Form-add-ingridients__input'
+            min="0"
+            
+            />
+                      <Autocomplete
+            onChange={handlerInputIng}
+            {...defaultProps}
+            disablePortal
+            id="combo-box-demo"
+            options={allIngridients}
+            sx={{ width: 300 }}
+            renderOption={(props, option) => (<Box data-value={JSON.stringify(option)} {...props} > {option.name} ({option.measure}) </Box>)}
+            renderInput={(params) => (<TextField  label="выберете ингредиент" 
+              {...params} inputProps={{...params.inputProps}}/>)}
+            /> 
+         
+            <Button className='Form-add-ingridients__btn'  variant="outlined">Добавить ингредиент</Button>
+         </form>
         {Object.keys(recipe).length !== 0 && (
         <ul className="home__list-cards cards-list">
             <li className="cards-list__item" id={recipe[0]['recipe_id']} key={uuidv4()}>
